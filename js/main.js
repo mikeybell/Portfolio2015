fc410665590269df9f39fce55ac5441aab428300
mikeybell <michaelrobertbell@gmail.com>
2015-06-24 21:23:21 -0400

    In class js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+alert('Hello World!');
+
+alert(40+84-23);
+
+var myHeading = document.querySelector('h4');
+myHeading.innerHTML = 'GOODBYE!';
+
+document.querySelector(".name").innerHTML = "GOODBYE!";
+
+document.querySelector("#name").innerHTML = "SEE YA!";
+
+
